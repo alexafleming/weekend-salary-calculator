87f3746a5f97e6103d3f8fb52228c923b5ce4962
Alexa Fleming <alexaflemingmn@gmail.com>
2023-07-18 17:29:16 -0500

    got forms to submit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,18 @@
-console.log("Hi!");
+console.log("hi");
+
+let monthlyCosts = 0;
+let forms = [];
+
+function completedForms(event) {
+    event.preventDefault();
+
+    forms.push({
+        firstName: firstName.value,
+        lastName: lastName.value,
+        idNumber: idNumber.value,
+        jobTitle: jobTitle.value,
+        annualSalary: annualSalary.value
+    });
+
+    console.log(forms);
+}
