ef6360a9a49de552b0822330c1f40a8bea4d9a98
Alexa Fleming <alexaflemingmn@gmail.com>
2023-07-18 19:06:33 -0500

    added employees table

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,4 +15,21 @@ function completedForms(event) {
     });
 
     console.log(forms);
+
+    let employeeInformation = document.getElementById('employees-information');
+    let row = employeeInformation.insertRow(forms.length);
+
+    let row1 = row.insertCell(0);
+    let row2 = row.insertCell(1);
+    let row3 = row.insertCell(2);
+    let row4 = row.insertCell(3);
+    let row5 = row.insertCell(4);
+    let row6 = row.insertCell(5);
+
+    row1.innerHTML = firstName.value;
+    row2.innerHTML = lastName.value;
+    row3.innerHTML = idNumber.value;
+    row4.innerHTML = jobTitle.value;
+    row5.innerHTML = annualSalary.value;
+    row6.innerHTML = "delete";
 }
